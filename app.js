215d71f1f9bd15c2e9964282e1dead3e4a35a605
Rayan Mamhoud <kiraclover@protonmail.com>
2025-05-14 16:50:38 +0100

    Edited app.js

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ app.post('/api/post', async (req, res)=> {
 })
 
 app.listen(3000, (req, res)=>{
-    console.log('Server started at http://127.0.0.1:3000')
+    console.log('Server started!')
 })
 
 
